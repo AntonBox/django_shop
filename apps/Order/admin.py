6c61025b35b865af446c8dcd4ed1d40b5ce08399
AntonBox <antonbox94@gmail.com>
2016-11-13 15:19:56 +0200

    template without order

--- apps/Order/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from apps.Order import models
-
-# Register your models here.
-admin.site.register(models.Order)
--- apps/Order/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from root.base_models import TimedModel
-
-
-class Order(TimedModel):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=13)
-    address = models.CharField(max_length=200)
