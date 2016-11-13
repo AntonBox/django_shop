3ed31c6c3aa3b2233464f705e89ac517bae650d4
AntonBox <antonbox94@gmail.com>
2016-11-13 22:56:28 +0200

    order fixed clear

--- apps/Order/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic.base import TemplateView
-# Create your views here.
-
-
-class TestView(TemplateView):
-    template_name = 'index.html'
--- root/urls.py
+++ root/urls.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^order/', include('apps.Order.urls')),
     url(r'^admin/', admin.site.urls),
 ]
