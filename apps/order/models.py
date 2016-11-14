c515887a24d0cfa370af97fe1f32d7f0b04f04a1
AntonBox <antonbox94@gmail.com>
2016-11-14 13:03:54 +0200

    final commit

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from root.base_models import TimedModel
+from django.contrib.auth.models import User
 
 
 class Order(TimedModel):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User)
     # cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=255)
