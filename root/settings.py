850e5533502add978e0723efd99c5eca385e1446
Andrey Sklyarov <AndrewNikon@mail.ru>
2016-11-16 21:20:30 +0200

    Revert "model fix conflicts"
    This reverts commit 4458bd7000284d08464d5eb95cc5815812406313.
    
    Conflicts:
    	root/local_settings.py.default

--- root/settings.py
+++ root/settings.py
@@ -81,8 +81,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'shop',
+        'PASSWORD': 'shop',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
