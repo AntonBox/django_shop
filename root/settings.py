ee3517ca18285752f757bfbf8ddacd79dcb10dd9
Andrey Sklyarov <AndrewNikon@mail.ru>
2016-11-23 21:58:19 +0200

    Add mediafiles serving. Fix apps config.

--- root/settings.py
+++ root/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.order'
+    'apps.order',
     'apps.cart',
     'apps.catalog',
 ]
