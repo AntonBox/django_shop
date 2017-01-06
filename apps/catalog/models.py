8c782cf0a07e6c12b582a8683778093467bcbaa9
AntonBox <antonbox94@gmail.com>
2017-01-06 20:59:23 +0200

    blank for page catalog

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class Category(TimedModel):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
-    image = models.ImageField(upload_to='img')
+    image = models.ImageField(upload_to='categories')
     description = models.TextField()
 
     class Meta:
@@ -20,15 +20,16 @@ class Category(TimedModel):
         return reverse('view_category', kwargs={'slug': self.slug})
 
 
-class Product (TimedModel):
-    def get_image_path(instance, filename):
-        return 'products_img/{}/{}/{}/{}'.format(datetime.now().year,
+def get_image_path(instance, filename):
+    return 'products_img/{0}/{1}/{2}/{3}'.format(datetime.now().year,
                                                  datetime.now().month,
                                                  datetime.now().day,
-                                                 filename,)
+                                                 filename)
 
+
+class Product (TimedModel):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, related_name='products',)
+    category = models.ForeignKey(Category, related_name='products')
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     quantity = models.PositiveIntegerField()
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,47 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from apps.catalog.models import Product, Category
 
-'''
-def catalog(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'catalog.html', {
-        'products': products,
-        'categories': categories
-    })
-
-
-def view_category(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    return render(request, 'catalog.html', {
-        'categories': Category.objects.all(),
-        'products': Product.objects.filter(category=category)
-    })
-'''
-
-'''
-def products(request, slug=None):
-    if (slug is not None):
-        category = get_object_or_404(Category, slug=slug)
-        return render(request, 'catalog.html', {
-            'categories': Category.objects.all(),
-            'products': Product.objects.filter(category=category)
-        })
-    else:
-        products = Product.objects.all()
-        categories = Category.objects.all()
-        return render(request, 'catalog.html', {
-            'products': products,
-            'categories': categories
-        })
-'''
-
 
 def products(request, slug=None):
     products = Product.objects.all()
     categories = Category.objects.all()
     if slug:
         category = get_object_or_404(Category, slug=slug)
-        products = Product.objects.filter(category=category)
+        products = products.filter(category=category)
     return render(request, 'catalog.html', {'products': products,
                                             'categories': categories})
