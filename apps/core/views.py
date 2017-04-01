5c5e50ea9ed08615c90978be9ec7da3a6a7c82a2
AntonBox <antonbox94@gmail.com>
2017-04-01 17:18:27 +0300

    parallax main

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,6 +7,6 @@ def index(request):
     visuals = Carousel.objects.order_by('index').filter(is_active=True).all()
     products = Product.objects.order_by('created_at').all()[:4]
     categories = Category.objects.all()
-    return render(request, 'main.html', {'visuals': visuals,
-                                         'products': products,
-                                         'categories': categories})
+    return render(request, 'main2.html', {'visuals': visuals,
+                                          'products': products,
+                                          'categories': categories})
