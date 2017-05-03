511e0389af21439980303374474bddb08aa7562c
AntonBox <antonbox94@gmail.com>
2017-05-03 23:18:55 +0300

    added checking unique auth users in order

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from apps.cart.models import Cart
+import itertools
 import random
 import string
 
@@ -11,14 +12,21 @@ class User(AbstractUser):
         cart, _ = Cart.objects.get_or_create(user=self, status=Cart.OPEN)
         return cart
 
-    def create_user_order(first_name, last_name, email):
-        username = str(last_name) + str(random.randint(1, 99))
+    @classmethod
+    def create_user_order(cls, first_name, last_name, email):
+        username = email.split('@')[0]
+        extra_username = ''
+        for x in itertools.count(1):
+            if not User.objects.filter(username=username + extra_username).exists():
+                break
+            extra_username = str(x)
+            username = username + extra_username
         password = ''.join(random.choice(
             string.ascii_uppercase +
             string.ascii_lowercase + string.digits) for x in range(10))
-        user = User.objects.create_user(username=username,
-                                        password=password,
-                                        first_name=first_name,
-                                        last_name=last_name,
-                                        email=email)
+        user = cls.objects.create_user(username=username,
+                                       password=password,
+                                       first_name=first_name,
+                                       last_name=last_name,
+                                       email=email)
         return user
