7bfe20d7c12ec9ef65daa2161bd4d5602e11ed68
AntonBox <antonbox94@gmail.com>
2017-05-04 22:55:40 +0300

    added navigation

--- apps/testview/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestviewConfig(AppConfig):
-    name = 'testview'
--- root/urls.py
+++ root/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^btemp/', include('apps.testview.urls')),
     url(r'^catalog/', include('apps.catalog.urls')),
     url(r'^cart/', include('apps.cart.urls')),
     url(r'^order/', include('apps.order.urls')),
